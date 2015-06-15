b6357d1268d5b86c7cfb3a03487e6971e354bae8
Andriy Ermolenko <andriy.ermolenko@gmail.com>
2015-06-15 18:00:46 +0300

    enhanced pickUp method for different separators

--- parse.js
+++ parse.js
@@ -3,15 +3,16 @@
  */
 
 var parser = require('./lib/sqlParser').parse;
+var whereParse = require('./lib/sqlParseTool').parseDeepWhere;
 
 var sql = 'SELECT csv_adminAudit.Parameter_Modified, csv_adminAudit.Value' +
 ' FROM dbo.csv_adminAudit ' +
-'where csv_adminAudit.Parameter_Modified = 2';
+'where csv_adminAudit.Parameter_Modified = 2 and some = 3';
 
 var sql2 = 'SELECT count(ppa) as aaax, csv_adminAudit.Parameter_Modified, csv_adminAudit.Value' +
     ' FROM dbo.csv_adminAudit ' +
-    'where (csv_adminAudit.Parameter_Modified > 2) and (csv_adminAudit.Parameter_Modified < 200)';
+    'where (csv_adminAudit.Parameter_Modified > 2) or (csv_adminAudit.Parameter_Modified < 200)';
 
 var obj = parser(sql);
-console.log(JSON.stringify(obj));
+//console.log(JSON.stringify(obj));
 console.log(JSON.stringify(obj[0].where));
